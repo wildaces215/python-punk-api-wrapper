9a4523775d34e025ed099bfcdded4ecc7a9eb61b
wildaces215 <wannbecoastie@gmail.com>
2018-10-09 14:23:08 -0400

    Removed files and added functions for dates

--- beer.py
+++ beer.py
@@ -1,18 +1,34 @@
-
 import requests
 
-url = 'https://api.punkapi.com/v2/'
+url = 'https://api.punkapi.com/v2/beers'
 
 
 
 
+
+
+#Gets a random Beer 
 def random_beer():
-    r = requests.get(url+'/beers/random')
+    r = requests.get(url+'/random')
     return r.text
 
+def whiteToUnder(userString):
+    return userString.replace(" ","_")
 
+
+#Gets the beer names from api, uses partial strings.
 def beer_name_data(beer_name):
-    res = requests.get(url+'/beers?beer_name='+beer_name)
+    res = requests.get(url+'?beer_name='+beer_name)
     return res.text
 
+#Takes the parameters before date brewed
+def brewDateBefore(stringBef):
+    res = requests.get(url+'?brewed_before='+stringBef)
+    if res.status_code ==400:
+        return res.status_code
+    else:
+        return res.text
 
+def brewDateAfter(afterDate):
+    res = requests.get(url+'?brewed_after='+afterDate)
+    return res.text
